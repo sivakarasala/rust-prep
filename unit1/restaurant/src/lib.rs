97b771ce23a163c594707e29ec999537b3134144
Siva Krishna <shivataraya@gmail.com>
2019-11-14 21:10:04 +0000

    modules part-2

--- unit1/restaurant/src/lib.rs
+++ unit1/restaurant/src/lib.rs
@@ -1,17 +1,44 @@
-mod front_of_house {
+pub mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
 
-        fn serve_order() {}
+        pub fn serve_order() {}
 
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+            seasonal_fruit: String,
+        }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("mangoes"),
+            }
+        }
+    }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
 }
 
 pub fn eat_at_restaurant() {
@@ -20,4 +47,14 @@ pub fn eat_at_restaurant() {
 
         // Relative path
         front_of_house::hosting::add_to_waitlist();
+
+        // Order a breakfast in the summer with Rye toast
+        let mut meal = back_of_house::Breakfast::summer("Rye");
+        // Change our mind about what bread we'd like
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please", meal.toast);
+
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
 }
+
