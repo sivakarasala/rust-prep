d10787c14eac39dfd2cbdae00d1e351b3e1138a4
Siva Krishna <shivataraya@gmail.com>
2019-11-18 03:51:45 +0000

    collections::HashMap - 1

---
+++ unit1/collections/src/hashmaps.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+
+pub fn hash_map() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{:?}", scores);
+}
--- unit1/collections/src/main.rs
+++ unit1/collections/src/main.rs
@@ -1,10 +1,13 @@
 mod vectors;
 mod strings;
+mod hashmaps;
 
 use vectors::vector_type;
 use strings::string_type;
+use hashmaps::hash_map;
 
 fn main() {
     vector_type();
     string_type();
+    hash_map();
 }
