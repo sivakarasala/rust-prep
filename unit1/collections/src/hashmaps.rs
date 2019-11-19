00f3db728b6405255b33b576220af0c8d240b00a
Siva Krishna <shivataraya@gmail.com>
2019-11-19 22:03:39 +0000

    HashMap - 2

--- unit1/collections/src/hashmaps.rs
+++ unit1/collections/src/hashmaps.rs
@@ -7,4 +7,47 @@ pub fn hash_map() {
     scores.insert(String::from("Yellow"), 50);
 
     println!("{:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    println!("{:?}", scores);
+
+    let field_name = String::from("Favorite Color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{}, {}", key, value);
+    }
+
+    let mut scores = HashMap::new();
+    scores.insert("Blue".to_string(), 10);
+
+    scores.entry("Yellow".to_string()).or_insert(50);
+    scores.entry("Blue".to_string()).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "aum namah shivaya";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}, {}", map, map.len());
 }
